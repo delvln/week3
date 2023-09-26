03be7bc61dd0c9c1403dd5253fa7cf43197fcfc4
delvln <144612317+delvln@users.noreply.github.com>
2023-09-26 23:53:05 +0900

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,20 @@
-
+total = 0
+count = 0
+while True:
+    #enter done to finish
+    user_input = input("Enter a number: ")
+    if user_input == "done":
+        break
+    try:
+        number = float(user_input)
+        total += number
+        count += 1
+    except ValueError:
+        print("invalid input, enter a number")
+if count > 0:
+    average = total / count
+    print("Sum of input numbers:", total)
+    print("number of input :", count)
+    print("Average of input numbers:", average)
+else:
+    print("No input")
